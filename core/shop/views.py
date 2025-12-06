bdd7ccad33902c070584790cf6f7cf8bc15d5b1d
sinamarefat <sina.marefat82@gmail.com>
2025-12-06 21:06:33 +0330

    4-4
    ایجاد فیکر برای ایجاد پست تصادفی جهت تست

--- core/shop/views.py
+++ core/shop/views.py
@@ -12,30 +12,30 @@ from django.http import JsonResponse
 # # Create your views here.
 
 
-# class ShopProductGridView(ListView):
-#     template_name = "shop/product-grid.html"
-#     paginate_by = 9
+class ShopProductGridView(ListView):
+    template_name = "shop/product-grid.html"
+    paginate_by = 9
 
-#     def get_paginate_by(self, queryset):
-#         return self.request.GET.get('page_size', self.paginate_by)
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get('page_size', self.paginate_by)
 
-#     def get_queryset(self):
-#         queryset = ProductModel.objects.filter(
-#             status=ProductStatusType.publish.value)
-#         if search_q := self.request.GET.get("q"):
-#             queryset = queryset.filter(title__icontains=search_q)
-#         if category_id := self.request.GET.get("category_id"):
-#             queryset = queryset.filter(category__id=category_id)
-#         if min_price := self.request.GET.get("min_price"):
-#             queryset = queryset.filter(price__gte=min_price)
-#         if max_price := self.request.GET.get("max_price"):
-#             queryset = queryset.filter(price__lte=max_price)
-#         if order_by := self.request.GET.get("order_by"):
-#             try:
-#                 queryset = queryset.order_by(order_by)
-#             except FieldError:
-#                 pass
-#         return queryset
+    def get_queryset(self):
+        queryset = ProductModel.objects.filter(
+            status=ProductStatusType.publish.value)
+        if search_q := self.request.GET.get("q"):
+            queryset = queryset.filter(title__icontains=search_q)
+        if category_id := self.request.GET.get("category_id"):
+            queryset = queryset.filter(category__id=category_id)
+        if min_price := self.request.GET.get("min_price"):
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price := self.request.GET.get("max_price"):
+            queryset = queryset.filter(price__lte=max_price)
+        if order_by := self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+        return queryset
 
 #     def get_context_data(self, **kwargs):
 #         context = super().get_context_data(**kwargs)
@@ -93,9 +93,9 @@ from django.http import JsonResponse
 
 #         return JsonResponse({"message": message})
 
-from django.shortcuts import render
+# from django.shortcuts import render
 
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 
-class ShopProductGridView(TemplateView):
-    template_name="shop/product-grid.html"
+# class ShopProductGridView(TemplateView):
+#     template_name="shop/product-grid.html"
