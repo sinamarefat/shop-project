372dd9aafebd9b5df9ca4e652f5d42777c27e09b
sinamarefat <sina.marefat82@gmail.com>
2025-12-08 06:09:19 +0330

    4-15 Add latest and similar products template tags
    Introduced custom template tags for displaying latest and similar products. Updated product detail and index templates to use these tags and moved product card grids to separate include files for better reusability and maintainability. Also updated logo links in base template to use Django URL tags. اضافه کردن تگ برای محصولات با تگ مشترک کتگوری

---
+++ core/shop/templatetags/shop_tags.py
@@ -0,0 +1,24 @@
+from django import template
+from shop.models import ProductStatusType, ProductModel,WishlistProductModel
+
+register = template.Library()
+
+
+
+@register.inclusion_tag("includes/latest-products.html")
+def show_latest_products():
+    latest_products = ProductModel.objects.filter(
+        status=ProductStatusType.publish.value
+    ).distinct().order_by("-created_date")[:8]
+
+    return {"latest_products": latest_products}
+
+
+@register.inclusion_tag("includes/similar-products.html")
+def show_similar_products(product):
+    # request = context.get("request")
+    product_categories= product.category.all()
+    similar_prodcuts = ProductModel.objects.filter(
+        status=ProductStatusType.publish.value,category__in=product_categories).order_by("-created_date")[:4]
+    # wishlist_items =  WishlistProductModel.objects.filter(user=request.user).values_list("product__id",flat=True) if request.user.is_authenticated else []
+    return {"similar_prodcuts": similar_prodcuts}
