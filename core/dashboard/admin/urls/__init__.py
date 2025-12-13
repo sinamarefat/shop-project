81df49d63e33c99d7bd744b4a39c8721c274ab9b
sinamarefat <sina.marefat82@gmail.com>
2025-12-13 23:02:37 +0330

    7 تنظیمات داشبورد ادمین
    ایحاد ادیت و حذف و ....

--- core/dashboard/admin/urls/__init__.py
+++ core/dashboard/admin/urls/__init__.py
@@ -5,7 +5,7 @@ app_name = "admin"
 urlpatterns = [
     path("",include("dashboard.admin.urls.generals")),
     path("",include("dashboard.admin.urls.profiles")),
-    # path("",include("dashboard.admin.urls.products")),
+    path("",include("dashboard.admin.urls.products")),
     # path("",include("dashboard.admin.urls.orders")),
     # path("",include("dashboard.admin.urls.reviews")),
     # path("",include("dashboard.admin.urls.newsletters")),
